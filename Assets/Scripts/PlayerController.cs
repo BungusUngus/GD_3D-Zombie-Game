817026237a664bac050aaeb3e597229e86c35366
Domino-111 <dom.walsh@outlook.com>
2024-11-08 12:49:43 +1100

    Move script modified to include rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,32 @@ using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
+    public CharacterController controller;
+
     public float speed;
+    public float rotationSpeed;
 
     Vector3 moveDirection;
     Vector2 input;
+
+    private Vector3 rotation;
 
     void Start()
     {
         
     }
 
-    void Update()
+    public void Update()
+    {
+        this.rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime, 0);
+
+        Vector3 move = new Vector3(0, 0, Input.GetAxisRaw("Vertical") * Time.deltaTime);
+        move = this.transform.TransformDirection(move);
+        controller.Move(move * speed);
+        this.transform.Rotate(this.rotation);
+    }
+
+    /*void Update()
     {
         //Get movement inputs for the frame
         input.x = Input.GetAxis("Horizontal");
@@ -27,5 +42,5 @@ public class PlayerController : MonoBehaviour
 
         //Move based on moveDirection using time
         GetComponent<CharacterController>().Move(moveDirection * Time.deltaTime);
-    }
+    }*/
 }

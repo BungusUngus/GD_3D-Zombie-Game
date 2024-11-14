ef52fc89fb5e3f7c6e5de03422076b22f1b7b9d5
Domino-111 <dom.walsh@outlook.com>
2024-11-15 10:33:35 +1100

    Addition of remaining AI scripts

--- Assets/Scripts/AILogic.cs
+++ Assets/Scripts/AILogic.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
 
 public class AILogic : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Reference to other things
+    public Transform key;
+    public DoorLogic door;
+    public Transform player;
+
+    //Self Reference
+    public RandomWalk randomWalk;
+    private NavMeshAgent _agent;
+
     void Start()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (door.state == DoorLogic.DoorStates.Locked && key != null)
+        {
+            _agent.destination = key.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (key.gameObject == other.gameObject)//other.GetComponent<Key>() != null)
+        {
+            door.unlockDoor = true;
+            Destroy(other.gameObject);
+        }
     }
 }
